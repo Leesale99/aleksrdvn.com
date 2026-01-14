21d7325fbd4348ab50ad904a478c096c2d2e4e15
Aleksandar Radovanovic <aleks.rdvn@gmail.com>
2026-01-14 15:49:52 +0100

    feat: add counter

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,9 @@
+import { Counter } from "../components/FlawedCounter";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Hello, World!
+      <Counter initial={0} />
     </div>
   )
 }
